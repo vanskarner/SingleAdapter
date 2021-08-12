335597c997dab5de70e9c6851646a4fafe2d3698
vanskarner <luisolazolujan@gmail.com>
2021-08-11 21:16:06 -0500

    applying the basis for pagination

---
+++ Adapters/app/src/main/java/com/vanskarner/adapters/ui/BaseActivity.java
@@ -0,0 +1,21 @@
+package com.vanskarner.adapters.ui;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    protected abstract int setLayout();
+
+    protected abstract void setupView();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(setLayout());
+        setupView();
+    }
+
+}
--- Adapters/app/src/main/java/com/vanskarner/adapters/ui/BasePaginationActivity.java
+++ Adapters/app/src/main/java/com/vanskarner/adapters/ui/BasePaginationActivity.java
@@ -1,31 +1,44 @@
 package com.vanskarner.adapters.ui;
 
+import android.os.Bundle;
 import android.widget.Filter;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.Nullable;
 import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.RecyclerView;
 
-public abstract class BasePaginationActiviy extends AppCompatActivity {
+public abstract class BasePaginationActivity extends BaseActivity {
 
     protected int pageNumber = 1;
     protected boolean isLoading = false;
     protected boolean isFiltering = false;
 
-    protected abstract int lastVisibleItemPosition();
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setupAdapter();
+        setupSearcher();
+    }
 
-    protected abstract void loadMore();
+    protected abstract void setupAdapter();
+
+    protected abstract void setupSearcher();
+
+    protected abstract int setLastVisibleItemPosition(RecyclerView.LayoutManager layoutManager);
 
     protected abstract int setListSize();
+
+    protected abstract void loadMore();
 
     protected RecyclerView.OnScrollListener recyclerOnScrollListener() {
         return new RecyclerView.OnScrollListener() {
             @Override
             public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
                 super.onScrolled(recyclerView, dx, dy);
-                if (!isLoading && !isFiltering && recyclerView.getLayoutManager() != null &&
-                        lastVisibleItemPosition() == setListSize() - 1) {
+                RecyclerView.LayoutManager manager = recyclerView.getLayoutManager();
+                if (!isLoading && !isFiltering && manager != null &&
+                        setLastVisibleItemPosition(manager) == setListSize() - 1) {
                     pageNumber++;
                     loadMore();
                     isLoading = true;
@@ -38,7 +51,7 @@ public abstract class BasePaginationActiviy extends AppCompatActivity {
         return new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-                isFiltering = true;//*
+                isFiltering = true;
                 return false;
             }
 
@@ -51,5 +64,11 @@ public abstract class BasePaginationActiviy extends AppCompatActivity {
         };
     }
 
+    protected SearchView.OnCloseListener searchViewCloseListener() {
+        return () -> {
+            isFiltering = false;
+            return false;
+        };
+    }
 
 }
