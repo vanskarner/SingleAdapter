02087ddafbd5ec1468a0b3e14ce520f1c2095b9a
Vanskarner <vanskarner@gmail.com>
2021-12-16 21:35:16 -0500

    Adapters: developing examples of Progress

--- Adapters/app/src/main/java/com/vanskarner/adapters/examples/progress/ProgressActivity.java
+++ Adapters/app/src/main/java/com/vanskarner/adapters/examples/progress/ProgressActivity.java
@@ -1,5 +1,6 @@
 package com.vanskarner.adapters.examples.progress;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -13,28 +14,33 @@ import com.vanskarner.adapters.R;
 import com.vanskarner.adapters.common.adaptersothers.SingleAdapter;
 import com.vanskarner.adapters.examples.DataProvider;
 import com.vanskarner.adapters.examples.WomanModel;
-import com.vanskarner.adapters.ui.NoPages;
 
-import java.util.ArrayList;
+
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+import io.reactivex.Single;
 import io.reactivex.SingleObserver;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class ProgressActivity extends AppCompatActivity implements Pagination.OnLoadMoreListener {
+    private static String TAG = "DEBUGPROGRES";
 
     Pagination pagination = new Pagination(this, Pagination.LAST_POSITION);
-    List<WomanModel> list = new ArrayList<>();
+    RecyclerView recyclerView;
+    List<WomanModel> list = DataProvider.sampleDataMsg("- 1 -");
     SingleAdapter singleAdapter = new SingleAdapter();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.progress_activity);
-        RecyclerView recyclerView = findViewById(R.id.recycler);
+        recyclerView = findViewById(R.id.recycler);
         singleAdapter.add(new WomanAdapter());
         singleAdapter.add(R.layout.item_loading);
-        //singleAdapter.changeList(DataProvider.sampleDataMsg("- 1 -"));
+        singleAdapter.setList(list);
         recyclerView.setAdapter(singleAdapter);
         recyclerView.addOnScrollListener(pagination);
     }
@@ -45,11 +51,15 @@ public class ProgressActivity extends AppCompatActivity implements Pagination.On
                 .show();
     }
 
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    @SuppressLint("CheckResult")
     @Override
     public void onLoadMore(int page) {
         singleAdapter.showProgress();
-        DataProvider
-                .sampleDataPaging(page)
+        Single.just(DataProvider.sampleData())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .delay(1, TimeUnit.SECONDS)
                 .subscribe(new SingleObserver<List<WomanModel>>() {
                     @Override
                     public void onSubscribe(@NonNull Disposable d) {
@@ -57,19 +67,16 @@ public class ProgressActivity extends AppCompatActivity implements Pagination.On
                     }
 
                     @Override
-                    public void onSuccess(@NonNull List<WomanModel> personModels) {
-                        pagination.isLoading = false;
-                        //singleAdapter.changeList(personModels);
+                    public void onSuccess(@NonNull List<WomanModel> list) {
+                        //singleAdapter.hideProgress();
+                        singleAdapter.setList(list);
                     }
 
                     @Override
                     public void onError(@NonNull Throwable e) {
-                        pagination.isLoading = false;
-                        singleAdapter.hideProgress();
-                        if (e instanceof NoPages) {
-                            showNoPages();
-                        }
+                        Log.d(TAG, e.getMessage());
                     }
                 });
+
     }
 }
